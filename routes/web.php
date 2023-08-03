6c1e9ad3402923d3b7dc6fecbc6845c1e3b1f761
mustaque365 <mustaque.sami@gmail.com>
2023-08-03 14:46:23 +0600

    Authentication completed

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-
+Route::middleware('auth','checkrole:admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+});
 require __DIR__.'/auth.php';
